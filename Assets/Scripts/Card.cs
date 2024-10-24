390c36f1f95a437212baddeda88277364aa85fd6
dreckTrinity <dreck@trinity.edu>
2024-10-23 19:49:49 -0500

    Card Movement

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,4 +7,15 @@ public abstract class Card : MonoBehaviour
     public string cardName;
     public string rarity;
     public abstract void display();
+
+    private void OnMouseDrag() {
+        Plane dragPlane = new Plane(Camera.main.transform.forward, transform.position);
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0.0f;
+        if (dragPlane.Raycast(camRay, out enter)) 
+        {
+            Vector3 fingerPosition = camRay.GetPoint(enter);
+            transform.position = fingerPosition;
+        }
+    }
 }
