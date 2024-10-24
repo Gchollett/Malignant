3ead810c058fe8ab8fb5aa839f1e6accea3c6a8b
dreckTrinity <dreck@trinity.edu>
2024-10-23 20:26:44 -0500

    Progress made on card snapping

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Card : MonoBehaviour
@@ -8,14 +9,31 @@ public abstract class Card : MonoBehaviour
     public string rarity;
     public abstract void display();
 
+    private bool position_locked = true;
+
+
     private void OnMouseDrag() {
+        position_locked = false;
+
+        //Movement Handling, Prolly not optimal
         Plane dragPlane = new Plane(Camera.main.transform.forward, transform.position);
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         float enter = 0.0f;
         if (dragPlane.Raycast(camRay, out enter)) 
         {
-            Vector3 fingerPosition = camRay.GetPoint(enter);
-            transform.position = fingerPosition;
+            Vector3 mousePosition = camRay.GetPoint(enter);
+            transform.position = mousePosition;
         }
     }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        Debug.Log("GOT HERE!");
+        if(other.gameObject.tag == "Card Snappable" && position_locked == false){
+            Debug.Log($"Snapping to Object {other.gameObject.name}");
+            position_locked = true;
+            transform.position = other.transform.position;
+        }
+    }
+
+    
 }
