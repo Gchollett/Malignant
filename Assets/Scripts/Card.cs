8f338ee27491818a77ce090fc30ffc0f19c89b57
garrett <garrettchollett@yahoo.com>
2024-10-25 12:52:07 -0500

    Fixing

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public abstract class Card : MonoBehaviour
@@ -7,4 +8,55 @@ public abstract class Card : MonoBehaviour
     public string cardName;
     public string rarity;
     public abstract void display();
+    public static CardGameManager gm;
+    private GameObject lane;
+
+    private bool position_found = false;
+    private bool card_locker = false;
+
+    void Start()
+    {
+        gm = CardGameManager.Instance;
+    }
+    private void OnMouseDrag() {
+        if(card_locker) return;
+        //gm.protag.Hand.Remove(gameObject);
+        //Movement Handling, Prolly not optimal
+        Plane dragPlane = new Plane(Camera.main.transform.forward, transform.position);
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float enter = 0.0f;
+        if (dragPlane.Raycast(camRay, out enter)) 
+        {
+            Vector3 mousePosition = camRay.GetPoint(enter);
+            transform.position = mousePosition;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if(position_found && lane){
+            Vector2 tgtPos = lane.transform.position;
+            transform.position = new Vector2(tgtPos.x,tgtPos.y - 1);
+            lane.gameObject.GetComponent<Lane>().cardInLane();
+            // gm.protag.Hand.
+            card_locker =  true;
+            position_found = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if(other.gameObject.tag == "Card Snappable" && !position_found){
+            Debug.Log($"Snapping to Object: {other.gameObject.name}");
+            position_found = true;
+            lane = other.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Card Snappable"){
+            other.gameObject.GetComponent<Lane>().cardRemovedFromLane();
+        }
+    }
+    
+     
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,16 +8,16 @@ public class Player : MonoBehaviour
     private int health = 10;
     private int pips = 0;
     public Creature[] Lanes;
-    public GameObject[] Hand;
+    public List<GameObject> Hand;
     private void FixedUpdate() {
-        if(transform.childCount != Hand.Length){
+        if(transform.childCount != Hand.Count){
             for(int i = 0; i < transform.childCount; i++){
                 Destroy(transform.GetChild(i).gameObject);
             }
-            for(int i = 0; i < Hand.Length; i++){
+            for(int i = 0; i < Hand.Count; i++){
                 GameObject card = Instantiate(Hand[i]);
                 card.transform.parent = gameObject.transform;
-                card.transform.position = gameObject.transform.position + new Vector3(2*(i-Hand.Length/2f+.5f),0,0);
+                card.transform.position = gameObject.transform.position + new Vector3(2*(i-Hand.Count/2f+.5f),0,0);
             }
         }
     }
