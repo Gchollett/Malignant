bbe76840426e6ea9a9b66c3d8cf589ce513c8129
dreckTrinity <dreck@trinity.edu>
2024-10-25 12:43:00 -0500

    Additional functionality to card drag

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -8,13 +8,19 @@ public abstract class Card : MonoBehaviour
     public string cardName;
     public string rarity;
     public abstract void display();
+    public static CardGameManager gm;
+    private GameObject lane;
 
-    private bool position_locked = true;
+    private bool position_found = false;
+    private bool card_locker = false;
 
-
+    void Start()
+    {
+        gm = CardGameManager.Instance;
+    }
     private void OnMouseDrag() {
-        position_locked = false;
-
+        if(card_locker) return;
+        //gm.protag.Hand.Remove(gameObject);
         //Movement Handling, Prolly not optimal
         Plane dragPlane = new Plane(Camera.main.transform.forward, transform.position);
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -26,14 +32,31 @@ public abstract class Card : MonoBehaviour
         }
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
-        if(other.gameObject.tag == "Card Snappable" && !position_locked){
-            Debug.Log($"Snapping to Object: {other.gameObject.name}");
-            position_locked = true;
-            Vector2 tgtPos = other.transform.position;
+    private void OnMouseUp()
+    {
+        if(position_found && lane){
+            Vector2 tgtPos = lane.transform.position;
             transform.position = new Vector2(tgtPos.x,tgtPos.y - 1);
-            other.gameObject.GetComponent<Lane>().cardPlayedInLane();
+            lane.gameObject.GetComponent<Lane>().cardInLane();
+            // gm.protag.Hand.
+            card_locker =  true;
+            position_found = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        if(other.gameObject.tag == "Card Snappable" && !position_found){
+            Debug.Log($"Snapping to Object: {other.gameObject.name}");
+            position_found = true;
+            lane = other.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Card Snappable"){
+            other.gameObject.GetComponent<Lane>().cardRemovedFromLane();
         }
     }
     
+     
 }
