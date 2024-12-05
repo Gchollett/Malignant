4f214688b0709b27ccb9a0327f6fbef399352895
dreckTrinity <dreck@trinity.edu>
2024-12-05 16:53:20 -0600

    Adding Basic Lighitng to Overworld. Updated Project Settings

--- Assets/Scripts/Overworld Scripts/Overworld Player.cs
+++ Assets/Scripts/Overworld Scripts/Overworld Player.cs
@@ -13,12 +13,8 @@ public class OverworldPlayer : MonoBehaviour
         mm = MapManager.Instance;
         currentNode = mm.nodes[0][0];
         Debug.Log($"Player started at node {currentNode}");
-        Debug.Log( currentNode.GetComponent<LineRenderer>().colorGradient.colorKeys[0].color);
-        currentNode.GetComponent<LineRenderer>().colorGradient.SetKeys(
-            new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.red, 1.0f) },
-            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
-        );
-        Debug.Log( currentNode.GetComponent<LineRenderer>().colorGradient.colorKeys[0].color);
+        currentNode.GetComponent<LineRenderer>().startColor = Color.red;
+        currentNode.GetComponent<LineRenderer>().endColor = Color.red;
     }
 
     // Update is called once per frame
