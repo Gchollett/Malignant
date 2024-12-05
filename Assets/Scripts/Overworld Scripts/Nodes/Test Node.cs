90883f64759373c811c15a9a63885f9f775ae037
dreckTrinity <dreck@trinity.edu>
2024-12-05 17:58:42 -0600

    Added basic lerp movement + moved movement logic to player script

--- Assets/Scripts/Overworld Scripts/Nodes/Test Node.cs
+++ Assets/Scripts/Overworld Scripts/Nodes/Test Node.cs
@@ -8,8 +8,7 @@ public class TestNode : MapNode
 {
     public override void OnVisit()
     {   
-        Debug.Log(mm);
-       mm.MovePlayer(gameObject.transform.position, this);
+       //mm.MovePlayer(gameObject.transform.position, this);
        SceneManager.LoadScene("Main Scene 1");
     }
 }
--- Assets/Scripts/Overworld Scripts/Overworld Player.cs
+++ Assets/Scripts/Overworld Scripts/Overworld Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class OverworldPlayer : MonoBehaviour
@@ -7,23 +8,60 @@ public class OverworldPlayer : MonoBehaviour
     public static OverworldPlayer Instance;
     MapManager mm;
     public MapNode currentNode;
+    public MapNode nextNode;
+    private Vector3 newLoc;
+    private Vector3 startPos;
+
+    public float movementTime = 3;
+    private float elapsedTime;
+    private bool isMoving = false;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void initializePlayer()
     {   
         Debug.Log("Running");
         mm = MapManager.Instance;
         currentNode = mm.nodes[0][0];
         Debug.Log($"Player started at node {currentNode}");
-        currentNode.GetComponent<LineRenderer>().startColor = Color.red;
-        currentNode.GetComponent<LineRenderer>().endColor = Color.red;
+        startPos = currentNode.transform.position;
     }
 
     // Update is called once per frame
-    private void Update() {
-        //Debug.Log("Player Update");
-        currentNode.GetComponent<LineRenderer>().colorGradient.SetKeys(
-            new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.red, 1.0f) },
-            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
-        );
+    private void FixedUpdate() {
+        if (isMoving) {
+            elapsedTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(startPos, newLoc, elapsedTime / movementTime);
+            if (elapsedTime >= movementTime) {
+                transform.position = newLoc;
+                elapsedTime = 0;
+                isMoving = false;
+                currentNode = nextNode;
+                startPos = nextNode.transform.position;
+            }
+        }
     }
 
+    public void NextNode(MapNode newNode) {
+        nextNode = newNode;
+        newLoc = nextNode.transform.position;
+        isMoving = true;
+    }
+
+    public bool ReadyToMove(MapNode newNode) {
+        if(!isMoving && newNode.edges.Contains(currentNode) || currentNode.edges.Contains(newNode)){
+            return true;
+        }
+        return false;
+    }
 }
