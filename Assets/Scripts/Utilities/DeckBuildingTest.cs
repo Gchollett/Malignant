50828474156559b547fec2369370eaca2a7f3434
garrett <garrettchollett@yahoo.com>
2024-12-06 13:09:51 -0600

    Small changes

--- Assets/Scripts/Utilities/DeckBuildingTest.cs
+++ Assets/Scripts/Utilities/DeckBuildingTest.cs
@@ -7,11 +7,24 @@ public class DeckBuildingTest : MonoBehaviour
 {
     private CardData[] CardObjects;
     private GameObject[] AbilityPrefabs;
-    public GameObject CardContent;
-    public GameObject AbilityContent;
+    public Transform CardContent;
+    public GameObject CardPrefab;
+    public Transform AbilityContent;
+    public GameObject AbilityPrefab;
+    public Transform DeckContent;
     void Start()
     {
         CardObjects = Resources.LoadAll<CardData>("ScriptableObjects/CreatureData");
         AbilityPrefabs = Resources.LoadAll<GameObject>("Prefabs/AbilityPrefabs");
+        foreach(CardData card in CardObjects){
+            GameObject new_card = Instantiate(CardPrefab,CardContent);
+            new_card.GetComponent<Card>().cardData = card;
+            new_card.transform.localScale = Vector2.one;
+            break;
+        }
+        foreach(GameObject ability in AbilityPrefabs){
+            GameObject new_ability = Instantiate(AbilityPrefab,AbilityContent);
+            new_ability.transform.localScale = Vector2.one;
+        }
     }
 }
